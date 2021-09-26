4da0e8de6ef69c74bdbb67bf21d6e3f19dad42c3
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-09-26 23:43:56 +0530

    First Commit

---
+++ src/main/java/com/example/greetingdb/GreetingdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.greetingdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/greetingdb/GreetingdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greetingdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
